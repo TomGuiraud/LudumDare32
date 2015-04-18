37e81308299511030e9a8a2bc6b5b6619b91ed92
TomGuiraud <tom_guiraud@hotmail.fr>
2015-04-18 23:12:50 +0200

    Base Code for RailRoad

---
+++ LudumDare32/Assets/Scripts/GameManager/GameManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public int _playerNumber = 4;
+	public GameObject _trainPrefab;
+	private GameObject[] _allTrains;
+
+	public static List<RailRoadScript> _railRoadScript;
+
+	// Use this for initialization
+	void Start () {
+		TrainInstantiation ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void TrainInstantiation (){
+		_allTrains = new GameObject[_playerNumber]; 
+		for (int i = 0; i < _playerNumber; i++) {
+			RailRoadScript tmpRailBlock = _railRoadScript[Random.Range(0, _railRoadScript.Count)];
+			print (this.transform.name + " : " + tmpRailBlock);
+
+			_allTrains[i] = Instantiate(_trainPrefab, tmpRailBlock.transform.position, Quaternion.identity) as 	GameObject;
+			TrainScript tmpTrainScript = _allTrains[i].GetComponent<TrainScript>();
+			tmpTrainScript._currentRailScript = tmpRailBlock;
+			tmpTrainScript.ComputeNextRailBlock(true);
+		}
+	}
+
+
+}
---
+++ LudumDare32/Assets/Scripts/Train/TrainScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainScript : MonoBehaviour {
+
+	//RailBlock
+	public RailRoadScript _previousRailScript;
+	public RailRoadScript _currentRailScript;
+
+	//Train Parameters_linkedRailScriptAvailable
+
+	public float _trainSpeed = 1.0f;
+	public float _trainRotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovementHandler ();
+
+		if (Vector3.Distance (this.transform.position, _currentRailScript.transform.position) < 0.1f) {
+			ComputeNextRailBlock(false);
+		}
+	}
+
+	//MOVEMENT------------------------------------------------------------------------------------------------
+
+	public void MovementHandler (){
+		this.transform.position = Vector3.MoveTowards (this.transform.position, _currentRailScript.transform.position, Time.deltaTime * _trainSpeed);						 
+	}
+
+	//ROTATION------------------------------------------------------------------------------------------------
+
+	public void RotationHandler (){
+		Vector3 relativePosition = _currentRailScript.transform.rotation.eulerAngles - this.transform.rotation.eulerAngles;
+		this.transform.rotation.eulerAngles = relativePosition;
+	}
+
+	//DESTINATION COMPUTING-----------------------------------------------------------------------------------
+
+	public void ComputeNextRailBlock (bool firstTime){
+		bool tmpAvailableBlock = false;
+		while (tmpAvailableBlock == false) {
+			RailRoadScript tmpRS = _currentRailScript._linkedRailRoadBlocks[Random.Range(0, _currentRailScript._linkedRailRoadBlocks.Count)];
+			print (this.transform.name + " : " + tmpRS);
+			if (firstTime || tmpRS != _previousRailScript){
+				_previousRailScript = _currentRailScript;
+				_currentRailScript = tmpRS;
+				RotationHandler();
+				tmpAvailableBlock = true;
+			}
+		}
+	}
+
+}
